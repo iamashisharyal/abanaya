9fce3ca65a4c5de6e8da4fbda925f7e796e30d91
iamashisharyal <138591030+iamashisharyal@users.noreply.github.com>
2024-07-09 13:53:40 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
